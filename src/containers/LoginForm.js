c701a77ee1e4d64450ae5806e4b86074d6cd57bd
lanebuleuse <aurore.fayet@gmail.com>
2020-09-28 15:00:45 +0200

    code connexion page done

---
+++ src/containers/LoginForm.js
@@ -0,0 +1,28 @@
+import { connect } from 'react-redux';
+import LoginForm from '../components/LoginForm';
+import { loginInputChange, loginInputSubmit, loginInputLogout } from '../actions/user';
+
+const mapState = (state) => ({
+  email: state.user.email,
+  password: state.user.password,
+  isLogged: state.user.isLogged,
+  loggedMessage: state.user.loggedMessage,
+  loading: state.user.loading,
+});
+
+const mapDispatch = (dispatch) => ({
+  changeField: (value, name) => {
+    const object = {
+      [name]: value,
+    };
+    dispatch(loginInputChange(object));
+  },
+  handleLogin: () => {
+    dispatch(loginInputSubmit());
+  },
+  handleLogout: () => {
+    dispatch(loginInputLogout());
+  },
+});
+
+export default connect(mapState, mapDispatch)(LoginForm);
---
+++ src/reducers/user.js
@@ -0,0 +1,54 @@
+import {
+  LOGIN_INPUT_CHANGE, LOGIN_INPUT_SUBMIT, LOGIN_SUCCESS, LOGIN_ERROR, LOGOUT_SUCCESS,
+} from '../actions/user';
+
+const initialState = {
+  loading: false,
+  email: 'acidman@herocorp.io',
+  password: 'fructis',
+  isLogged: false,
+  pseudo: '',
+  loggedMessage: 'Bienvenue intel',
+};
+
+const user = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case LOGOUT_SUCCESS:
+      return {
+        ...state,
+        isLogged: false,
+        pseudo: '',
+        loggedMessage: '',
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        isLogged: true,
+        pseudo: action.payload.pseudo,
+        loggedMessage: `Bienvenue ${action.payload.pseudo}`,
+      };
+    case LOGIN_ERROR:
+      return {
+        ...state,
+        loading: false,
+        isLogged: false,
+        pseudo: '',
+        loggedMessage: '',
+      };
+    case LOGIN_INPUT_CHANGE:
+      return {
+        ...state,
+        ...action.payload,
+      };
+    case LOGIN_INPUT_SUBMIT:
+      return {
+        ...state,
+        loading: true,
+      };
+    default:
+      return state;
+  }
+};
+
+export default user;
