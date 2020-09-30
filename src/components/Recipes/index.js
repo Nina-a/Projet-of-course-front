9414dee0c98d768de0771acb45383beb6ceea442
Nina <nina.leduc@gmail.com>
2020-09-30 16:56:33 +0200

    add recipes in state

--- src/components/Recipes/index.js
+++ src/components/Recipes/index.js
@@ -6,27 +6,34 @@ import PropTypes from 'prop-types';
 import './style.scss';
 
 // == Composant
-const Recipes = ({list}) => (
-  <div className="recipes">
-    <h1> Recipes </h1>
-    {list.map((recipeDetail) => (
-      <div className="card">
-        <img className="card-image" alt="illustration" src={recipeDetail.image} />
-        <div className="card-infos">
-          <button className="add planning" type="button">+</button>
-          <button className="add favorite" type="button">&#9825;</button>
-          <h3 className="card-title">{recipeDetail.title}</h3>
-          <a className="active">Lien vers la recette</a>
+const Recipes = ({ list }) => {
+  console.log('list', list);
+  return (
+    <div className="recipes">
+      <h1> Recipes </h1>
+      {
+      list.map((recipeDetail) => (
+        <div className="card">
+          <img className="card-image" alt="illustration" src={recipeDetail.image} />
+          <div className="card-infos">
+            <button className="add planning" type="button">+</button>
+            <button className="add favorite" type="button">&#9825;</button>
+            <h3 className="card-title">{recipeDetail.title}</h3>
+            <a className="active">Lien vers la recette</a>
+          </div>
         </div>
-      </div>
-    ))};
-  </div>
-);
+      ))
+      };
+    </div>
+  );
+};
 
 // == PropTypes
-Recipes.PropTypes = {
-  title: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
+Recipes.propTypes = {
+  list: PropTypes.arrayOf({
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+  }).isRequired,
 };
 // == Export
 export default Recipes;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import counter from './counter';
+import reducerrecipes from './recipes';
 
 export default combineReducers({
   counter,
+  reducerrecipes,
 });
