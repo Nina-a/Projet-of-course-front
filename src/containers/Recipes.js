bc7097df87ad7290f7563b29a13d10fe7459a6d0
Nina <nina.leduc@gmail.com>
2020-10-03 23:30:22 +0200

    add different list (listEntree, listPlat and listDessert) to dispatch in recipes)

--- src/containers/Recipes.js
+++ src/containers/Recipes.js
@@ -4,10 +4,10 @@ import Recipes from '../components/Recipes';
 
 // import reducerrecipes from '../reducers/recipes';
 
-const mapState = null;
-// console.log('state', state);
-// console.log('reducerrecipes', reducerrecipes);
-// console.log(state.reducerrecipes, 'state.reducerrecipes');
+const mapState = (state) => {
+  console.log(state);
+  
+};
 
 const mapDispatch = null;
 // == Besoin d'envoyer des actions jusqu'au reducer
--- src/reducers/recipes.js
+++ src/reducers/recipes.js
@@ -4,10 +4,12 @@ import {
 } from '../actions/recipes';
 
 export const initialState = {
+  listEntree: [],
+  listPlat: [],
+  listDessert: [],
   list: [],
   error: null,
   loading: false,
-
 };
 
 const reducerrecipes = (state = initialState, action = {}) => {
@@ -17,12 +19,33 @@ const reducerrecipes = (state = initialState, action = {}) => {
         ...state,
         error: 'Impossible de récupérer les recettes, vérifiez votre connexion',
       };
-    case FETCH_RECIPES_SUCCESS:
+    case FETCH_RECIPES_SUCCESS: {
+      const listRecipes = action.payload;
+      //debugger;
+      const listEntree = listRecipes.filter((recipeDetail) => {
+        const category = recipeDetail.categories[0].id === 1;
+        return category;
+      });
+
+      const listPlat = listRecipes.filter((recipeDetail) => {
+        const category = recipeDetail.categories[0].id === 2;
+        return category;
+      });
+
+      const listDessert = listRecipes.filter((recipeDetail) => {
+        const category = recipeDetail.categories[0].id === 3;
+        return category;
+      });
+
       return {
         ...state,
         list: [...action.payload],
+        listEntree: [...listEntree],
+        listPlat: [...listPlat],
+        listDessert: [...listDessert],
         error: null,
       };
+    }
     default:
       return state;
   }
