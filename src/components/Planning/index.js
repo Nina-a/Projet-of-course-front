2a10fbcc75b8b67925c59982f152268f19d19770
lanebuleuse <aurore.fayet@gmail.com>
2020-10-05 17:52:20 +0200

    planning css in progress

--- src/components/Planning/index.js
+++ src/components/Planning/index.js
@@ -6,98 +6,98 @@ import './style.css';
 
 // == Composant
 const Planning = () => (
-<div className="planning">
-	<section className="main">
-		<div className="rm-wrapper">
-			<div className="rm-content">
-				<h4>Lundi</h4>
-				<dl>
-					<dt>Midi</dt>
-					<dd>Entrée : salade de chèvre</dd>
-					<dd>Plat</dd>
-					<dd>Dessert</dd>
+  <div className="planning">
+    <section className="main">
+      <div className="rm-wrapper">
+        <div className="rm-content">
+          <h4>Lundi</h4>
+          <dl>
+            <dt>Midi</dt>
+            <dd>Entrée : salade de chèvre</dd>
+            <dd>Plat</dd>
+            <dd>Dessert</dd>
 
-					<dt>Soir</dt>
-					<dd>Entrée</dd>
-					<dd>Plat</dd>
-					<dd>Dessert</dd>
-				</dl>
-				<h4>Mardi</h4>
-				<dl>
-					<dt>Midi</dt>
-					<dd>Entrée : salade de chèvre</dd>
-					<dd>Plat</dd>
-					<dd>Dessert</dd>
+            <dt>Soir</dt>
+            <dd>Entrée</dd>
+            <dd>Plat</dd>
+            <dd>Dessert</dd>
+          </dl>
+          <h4>Mardi</h4>
+          <dl>
+            <dt>Midi</dt>
+            <dd>Entrée : salade de chèvre</dd>
+            <dd>Plat</dd>
+            <dd>Dessert</dd>
 
-					<dt>Soir</dt>
-					<dd>Entrée</dd>
-					<dd>Plat</dd>
-					<dd>Dessert</dd>
-				</dl>
-				<h4>Mercredi</h4>
-				<dl>
-					<dt>Midi</dt>
-					<dd>Entrée : salade de chèvre</dd>
-					<dd>Plat</dd>
-					<dd>Dessert</dd>
+            <dt>Soir</dt>
+            <dd>Entrée</dd>
+            <dd>Plat</dd>
+            <dd>Dessert</dd>
+          </dl>
+          <h4>Mercredi</h4>
+          <dl>
+            <dt>Midi</dt>
+            <dd>Entrée : salade de chèvre</dd>
+            <dd>Plat</dd>
+            <dd>Dessert</dd>
 
-					<dt>Soir</dt>
-					<dd>Entrée</dd>
-					<dd>Plat</dd>
-					<dd>Dessert</dd>
-				</dl>	
-				<h4>Jeudi</h4>
-				<dl>
-					<dt>Midi</dt>
-					<dd>Entrée : salade de chèvre</dd>
-					<dd>Plat</dd>
-					<dd>Dessert</dd>
+            <dt>Soir</dt>
+            <dd>Entrée</dd>
+            <dd>Plat</dd>
+            <dd>Dessert</dd>
+          </dl>
+          <h4>Jeudi</h4>
+          <dl>
+            <dt>Midi</dt>
+            <dd>Entrée : salade de chèvre</dd>
+            <dd>Plat</dd>
+            <dd>Dessert</dd>
 
-					<dt>Soir</dt>
-					<dd>Entrée</dd>
-					<dd>Plat</dd>
-					<dd>Dessert</dd>
-				</dl>	
-				<h4>Vendredi</h4>
-				<dl>
-					<dt>Midi</dt>
-					<dd>Entrée : salade de chèvre</dd>
-					<dd>Plat</dd>
-					<dd>Dessert</dd>
+            <dt>Soir</dt>
+            <dd>Entrée</dd>
+            <dd>Plat</dd>
+            <dd>Dessert</dd>
+          </dl>
+          <h4>Vendredi</h4>
+          <dl>
+            <dt>Midi</dt>
+            <dd>Entrée : salade de chèvre</dd>
+            <dd>Plat</dd>
+            <dd>Dessert</dd>
 
-					<dt>Soir</dt>
-					<dd>Entrée</dd>
-					<dd>Plat</dd>
-					<dd>Dessert</dd>
-				</dl>	
-				<h4>Samedi</h4>
-				<dl>
-					<dt>Midi</dt>
-					<dd>Entrée : salade de chèvre</dd>
-					<dd>Plat</dd>
-					<dd>Dessert</dd>
+            <dt>Soir</dt>
+            <dd>Entrée</dd>
+            <dd>Plat</dd>
+            <dd>Dessert</dd>
+          </dl>
+          <h4>Samedi</h4>
+          <dl>
+            <dt>Midi</dt>
+            <dd>Entrée : salade de chèvre</dd>
+            <dd>Plat</dd>
+            <dd>Dessert</dd>
 
-					<dt>Soir</dt>
-					<dd>Entrée</dd>
-					<dd>Plat</dd>
-					<dd>Dessert</dd>
-				</dl>
-				<h4>Dimanche</h4>
-				<dl>
-					<dt>Midi</dt>
-					<dd>Entrée : salade de chèvre</dd>
-					<dd>Plat</dd>
-					<dd>Dessert</dd>
+            <dt>Soir</dt>
+            <dd>Entrée</dd>
+            <dd>Plat</dd>
+            <dd>Dessert</dd>
+          </dl>
+          <h4>Dimanche</h4>
+          <dl>
+            <dt>Midi</dt>
+            <dd>Entrée : salade de chèvre</dd>
+            <dd>Plat</dd>
+            <dd>Dessert</dd>
 
-					<dt>Soir</dt>
-					<dd>Entrée</dd>
-					<dd>Plat</dd>
-					<dd>Dessert</dd>
-				</dl>
-			</div>
-		</div>
-	</section>
-</div>
+            <dt>Soir</dt>
+            <dd>Entrée</dd>
+            <dd>Plat</dd>
+            <dd>Dessert</dd>
+          </dl>
+        </div>
+      </div>
+    </section>
+  </div>
 );
 
 // == Export
