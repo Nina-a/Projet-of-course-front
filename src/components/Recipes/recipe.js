c6577537578e7bc59c72fb5b081976cacaa5e8a5
lanebuleuse <aurore.fayet@gmail.com>
2020-10-05 15:59:47 +0200

    card-actions js and css

--- src/components/Recipes/recipe.js
+++ src/components/Recipes/recipe.js
@@ -6,7 +6,12 @@ import PropTypes from 'prop-types';
 import './recipe.scss';
 
 // == Composant
-const CardRecipe = ({ recipe }) => {
+const CardRecipe = ({
+  recipe,
+  moreDetails,
+  addToFavorites,
+  addToPlanning,
+}) => {
   console.log(recipe);
   return (
     recipe.map((recipeDetail) => (
@@ -21,18 +26,35 @@ const CardRecipe = ({ recipe }) => {
               />
             </div>
             <div className="card-body">
-              <h3 className="font-weight-bold mb-3 pb-1">{recipeDetail.title}</h3>
+              <h3 className="font-weight-bold mb-3 pb-1">string.to{recipeDetail.title}</h3>
             </div>
           </div>
-
-          <div className="face back">
             <div className="card-body">
+              <div className="divbutton">
+                <button className="button-favorite" type="button" onClick={addToFavorites}> Coeur</button>
+                <select name={recipeDetail.title} onChange={addToPlanning}>
+                  <option value="0">Ajouter au planning</option>
+                  <option value="1">Lundi midi</option>
+                  <option value="2">Lundi soir</option>
+                  <option value="3">mardi midi</option>
+                  <option value="4">mardi soir</option>
+                  <option value="5">mercredi midi</option>
+                  <option value="6">mercredi soir</option>
+                  <option value="7">jeudi midi</option>
+                  <option value="8">jeudi soir</option>
+                  <option value="9">vendredi midi</option>
+                  <option value="10">vendredi soir</option>
+                  <option value="11">samedi midi</option>
+                  <option value="12">samedi soir</option>
+                  <option value="13">dimanche midi</option>
+                  <option value="14">dimanche soir</option>
+                </select>
+              </div>
               <a className="rotate-btn" data-card="card-1">
                 <i className="fas fa-undo" />
-                Plus de détails ici
+                <button className="button-details" type="button" onClick={moreDetails}>Plus de détail</button>
               </a>
             </div>
-          </div>
         </div>
       </div>
     ))
@@ -46,6 +68,9 @@ CardRecipe.propTypes = {
       picture: PropTypes.string.isRequired,
     }).isRequired,
   ),
+  moreDetails: PropTypes.func.isRequired,
+  addToPlanning: PropTypes.func.isRequired,
+  addToFavorites: PropTypes.func.isRequired, 
 };
 // == Export
 export default CardRecipe;
