81993903e85bb07450d264b9521dd590916c0462
Nina-a <nina.leduc@gmail.com>
2021-01-25 09:38:11 +0100

    page recette modifiée

--- src/store/index.js
+++ src/store/index.js
@@ -10,7 +10,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const enhancers = composeEnhancers(
   applyMiddleware(
-    //logMiddleware,
+    logMiddleware,
     ajaxMiddleware,
   ),
 );
