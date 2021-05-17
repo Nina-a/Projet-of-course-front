d1ffe503e99c4a6df7166ac604150123a122f9fe
Nina-a <nina.leduc@gmail.com>
2021-05-17 11:25:13 +0200

    nettoyage actions/recipes et containers/RecipeForm

--- src/containers/RecipeForm.js
+++ src/containers/RecipeForm.js
@@ -1,17 +1,12 @@
 import { connect } from 'react-redux';
 import RecipeForm from '../components/RecipeForm';
-// import { addRecipe } from '../actions/recipes';
 
 const mapState = (state) => ({
-  //listCategories: state.categories,
-  //listQuantity: state.quantity,
-  //listIngredients: state.ingredients,
+
 });
 
 const mapDispatch = (dispatch) => ({
-  sendForm: (payload) => {
-    dispatch(addRecipe(payload));
-  },
+
 });
 
 export default connect(mapState, mapDispatch)(RecipeForm);
